b8bed8f98c5d96f5b0b6060e08ab24303ef53eb8
Shoebillg <greerc@etown.edu>
2023-10-26 14:39:40 -0400

    making some changes to the map and pins

--- web_src/index.php
+++ web_src/index.php
@@ -6,27 +6,46 @@ require_once "includes/header.php";
 
 <!--<img src="images/CollegeMap.png">-->
 
-<img src="images/CollegeMap.png" alt="Map" usemap="#campusMap" width="1422" height="860">
+<img src="images/CollegeMap2.png" alt="Map" usemap="#campusMap" width="1094" height="754" style="position:relative">
+
+
 
 <map name="campusMap">
-<area shape="circle" coords="439,137,20" href="#brown" onclick="showBrown2('brown');" alt="BrownLot">
+<area shape="circle" coords="439,137,20" href="#brown" onclick="showBrown();" alt="BrownLot">
 <area shape="circle" coords="318,585,20" href="#Bretheran" onclick="pizza('church');" alt="BretheranChurch">
 <area shape="circle" coords="339,322,10" href="#Hoover" onclick="" alt="HooverLot">
 <area shape="circle" coords="585,483,30" href="#HackmanLots" onclick="" alt="HackmanLots">
 </map>
 
-<!--<p id="brown">
- <img src="images/lots/CollegeMapBrownLot.png">
-</p>-->
+<p id="brownPin2">
+ <img src="images/pinresized.jpg" id="brownPin" style="position: absolute; left: 429px; top: 207px; display:block;">
+</p>
+
+<p id="brown2">
+ <img src="images/lots/CollegeMapBrownLot.png" id="brown" style="position: absolute; left: 1094px; top: 80px; display:none;">
+</p>
+
+
+
 
 <script>
-        function showBrown(str) {
-            document.getElementById("brown").style.display='block';
-            brown.src = "images/lots/CollegeMapBrownLot.png"
+    var imgOn = false;
+        function showBrown() {
+            if ( imgOn ){
+                document.getElementById("brown").style.display='none';
+                imgOn = false;
+            }
+            else {
+                document.getElementById("brown").style.display='block';
+                imgOn = true;
+            }
+            
+            //brown.src = "images/lots/CollegeMapBrownLot.png";
         }
 
         function showBrown2(str) {
-            alert(str, <img src="images/lots/CollegeMapBrownLot.png">)
+            
+            alert(str);
         }
 
 
