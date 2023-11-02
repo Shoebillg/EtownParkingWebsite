f5b78c5f998a6821f772b3e740cff049c9a9517b
Nathaniel Brightup <brightupn@etown.edu>
2023-11-02 15:01:10 -0400

    Moved config

---
+++ includes/database_config.php
@@ -0,0 +1,9 @@
+<?PHP
+
+global $servername,$database,$username,$password,$GLOBAL_API_KEY;
+
+$servername = "127.0.0.1";
+$database = "etownparking";
+$username = "root";
+$password = "";
+$GLOBAL_API_KEY = "848429r2g";
