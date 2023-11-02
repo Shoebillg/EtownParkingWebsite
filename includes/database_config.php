79d98508ce87cb514df5ab02959fa99437374ad0
Nathaniel Brightup <brightupn@etown.edu>
2023-11-02 15:00:28 -0400

    Commented dropdowns for testing

--- includes/database_config.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-
-global $servername,$database,$username,$password,$GLOBAL_API_KEY;
-
-$servername = "127.0.0.1";
-$database = "etownparking";
-$username = "root";
-$password = "";
-$GLOBAL_API_KEY = "848429r2g";
