20f92e79e87ae2f2771156d80e64f97a65ca1ef2
yasudat83 <142951380+yasudat83@users.noreply.github.com>
2023-11-09 11:09:31 -0500

    Update read.php for parkingRules

--- data_src/api/parkingRule/read.php
+++ data_src/api/parkingRule/read.php
@@ -3,14 +3,15 @@ require_once "../../includes/database_config.php";
 require_once "../../classes/ParkingDatabase.php";
 
 
-$ruleID = isset($_GET["ruleID"])?$_GET["ruleID"]:"";
+//$ruleID = isset($_GET["ruleID"])?$_GET["ruleID"]:"";
 $typeID = isset($_GET["typeID"])?$_GET["typeID"]:"";
 $lotID = isset($_GET["lotID"])?$_GET["lotID"]:"";
 $timeID = isset($_GET["timeID"])?$_GET["timeID"]:"";
 $day = isset($_GET["day"])?$_GET["day"]:"";
-$desc = isset($_GET["desc"])?$_GET["desc"]:"";
+
+//$desc = isset($_GET["desc"])?$_GET["desc"]:"";
 //$badgeName;
-//$lotName, image, coor
+//$lotName, image, coor, time user choose
 
 $key = isset($_GET["APIKEY"])?$_GET["APIKEY"]:"";
 
@@ -23,7 +24,27 @@ if($key!=$GLOBAL_API_KEY){
 $where = "  ";
 $params = null;
 
-$sql = "Select * from parkingRules ".$where;
+/*if($lotID != ""){
+    $where = " where l.lotID = :lotid";
+    $params = [":lotid"=>$lotID];
+
+}*/
+if($typeID != ""){
+    $where = " where r.typeID = :typeid";
+    $params = [":typeid"=>$typeID];
+}
+else if($day != ""){
+    $where = " where day LIKE :day";
+    $params = [":day"=>"%".$day."%"];
+}
+else if($time != ""){
+    $where = "";
+    $params = [":time"=>$time];
+
+}
+//else if for time user choose
+
+$sql = "Select * from parkingRules r JOIN badgeTypes t ON r.typeID = t.typeID JOIN parkingLots l ON l.lotID = r.lotID JOIN parkingTimes p ON p.timeID = r.timeID ".$where;
 $data = ParkingDatabase::getDataFromSQL($sql, $params);
 
 echo json_encode($data);
