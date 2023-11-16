e8902934a85d29f1989f60804d92ba99c17e82b1
Nathaniel Brightup <brightupn@etown.edu>
2023-11-16 09:58:01 -0500

    Bugfixing create file

--- data_src/api/badgeType/create.php
+++ data_src/api/badgeType/create.php
@@ -4,8 +4,8 @@ require_once "includes/database_config.php";
 
 // Retrieve query parameters from the URL
 $key = isset($_POST["APIKEY"]) ? $_POST["APIKEY"] : "";
-$id = isset($_POST["typeID"]) ? intval($_POST["typeID"]) : "";
-$name = isset($_POST["name"]) ? intval($_POST["name"]) : 1;
+$id = isset($_POST["typeID"]) ? intval($_POST["typeID"]) : 1;
+$name = isset($_POST["name"]) ? $_POST["name"] : "";
 
 // Check if the provided API key matches the global API key
 if ($key != $GLOBAL_API_KEY) {
@@ -19,49 +19,33 @@ if ($id == "" || $id == 0) {
     exit;
 }
 
-// Prepare an array of parameters for SQL queries
-$params = [":id" => $id, ":name" => $name];
 
 try {
+    if ($id === 0) {
+        http_response_code(400); // Bad Request
+        echo json_encode(["message" => "No item under that ID"]);
+        exit;
+    }
+    // Prepare an array of parameters for SQL queries
+    $params = [":id" => $id, ":name" => $name];
     // Check if the id already exists in the table
     $sqlCheck = "SELECT * FROM badgeTypes WHERE typeID = :id AND name = :name";
-    $data = etownparking::getDataFromSQL($sqlCheck, [":id" => $id, ":name" => $name]);
+    $data = etownparking::getDataFromSQL($sqlCheck, $params);
 
-    if ($data !== false) {
+    if ($data === false) {
         // If the id doesn't exist in the table, insert it
         $sqlInsert = "INSERT INTO badgeTypes (typeID, name) VALUES (:id, :name)";
         etownparking::executeSQL($sqlInsert, $params);
+        http_response_code(200);
+        $message = ["message" => "ID Added to Table"];
+        echo json_encode($message);
     } else {
-        $message = ["message" => "Invalid entry"];
+        http_response_code(409); // Conflict
+        $message = ["message" => "ID already exists"];
         echo json_encode($message);
     }
-
-    // Respond with a success message and HTTP status code 200 (OK)
-    http_response_code(200);
-    $message = ["message" => "ID Added to Table"];
-    echo json_encode($message);
 } catch (PDOException $e) {
-    // If there's a database error, respond with an error message and HTTP status code 500 (Internal Server Error)
-    http_response_code(500);
+    http_response_code(500); // Internal Server Error
     echo json_encode(["message" => "Database Error: " . $e->getMessage()]);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
