2bd2e781105db34883e47fb7a0d2db3e8b7d599c
Nathaniel Brightup <brightupn@etown.edu>
2023-11-30 08:41:23 -0500

    Correcting minor spelling

--- web_src/editDatabase.php
+++ web_src/editDatabase.php
@@ -24,18 +24,20 @@ require_once "./includes/header.php";
 <!DOCTYPE html>
 <html lang="en">
 <LINK rel='stylesheet' href='css/editdb.css'>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Database</title>
 </head>
+
 <body>
     <header>
         <h1 style="background-color:transparent;">Edit Database</h1>
         <div>
-        <form method="post">
-            <input type="submit" name="logout" value="Logout" class="logout">
-        </form>
+            <form method="post">
+                <input type="submit" name="logout" value="Logout" class="logout">
+            </form>
         </div>
     </header>
 
@@ -43,12 +45,15 @@ require_once "./includes/header.php";
         <section id="badge">
             <h2>Badge Type Table</h2>
             <div>
-                <button class="add" id="addBadge" onclick="addBadge('<?php echo $url; ?>', '<?php echo $api_key; ?>')">Add to badge type table</button>
+                <button class="add" id="addBadge"
+                    onclick="addBadge('<?php echo $url; ?>', '<?php echo $api_key; ?>')">Add to badge type
+                    table</button>
             </div>
             <div id="addBadgeBox">
             </div>
             <div>
-                <button class="show"id="showBadge" type="button" onclick="badgeTableVisibility('<?php echo $url; ?>', '<?php echo $api_key; ?>')">
+                <button class="show" id="showBadge" type="button"
+                    onclick="badgeTableVisibility('<?php echo $url; ?>', '<?php echo $api_key; ?>')">
                     Show table!
                 </button>
             </div>
@@ -59,28 +64,32 @@ require_once "./includes/header.php";
         <section id="Lot">
             <h2>Parking Lot Table</h2>
             <div>
-                <button class="add" id="addLot"onclick="addLot('<?php echo $url; ?>', '<?php echo $api_key; ?>')">Add to parking lot table</button>
+                <button class="add" id="addLot" onclick="addLot('<?php echo $url; ?>', '<?php echo $api_key; ?>')">Add
+                    to parking lot table</button>
             </div>
             <div id="addLotBox">
             </div>
             <div>
-                <button class="show"id="showLot" type="button" onclick="lotTableVisibility('<?php echo $url; ?>', '<?php echo $api_key; ?>')">
+                <button class="show" id="showLot" type="button"
+                    onclick="lotTableVisibility('<?php echo $url; ?>', '<?php echo $api_key; ?>')">
                     Show table!
-                </button>   
+                </button>
             </div>
             <div id="lotTable" style="overflow-x:auto;">
             </div>
         </section>
 
         <section id="time">
-            <h2>Paking Time Table</h2>
+            <h2>Parking Time Table</h2>
             <div>
-                <button class="add" id="addTime" onclick="addTime('<?php echo $url; ?>', '<?php echo $api_key; ?>')">Add to parking time table</button>
+                <button class="add" id="addTime" onclick="addTime('<?php echo $url; ?>', '<?php echo $api_key; ?>')">Add
+                    to parking time table</button>
             </div>
             <div id="addTimeBox">
             </div>
             <div>
-                <button class="show" id="showTime" type="button" onclick="timeTableVisibility('<?php echo $url; ?>', '<?php echo $api_key; ?>')">
+                <button class="show" id="showTime" type="button"
+                    onclick="timeTableVisibility('<?php echo $url; ?>', '<?php echo $api_key; ?>')">
                     Show table!
                 </button>
             </div>
@@ -91,12 +100,14 @@ require_once "./includes/header.php";
         <section id="rule">
             <h2>Parking Rule Table</h2>
             <div>
-                <button class="add" id="addRule" onclick="addRule('<?php echo $url; ?>', '<?php echo $api_key; ?>')">Add to parking rule table</button>
+                <button class="add" id="addRule" onclick="addRule('<?php echo $url; ?>', '<?php echo $api_key; ?>')">Add
+                    to parking rule table</button>
             </div>
             <div id="addRuleBox">
             </div>
             <div>
-                <button class="show" id="showRule" type="button" onclick="ruleTableVisibility('<?php echo $url; ?>', '<?php echo $api_key; ?>')">
+                <button class="show" id="showRule" type="button"
+                    onclick="ruleTableVisibility('<?php echo $url; ?>', '<?php echo $api_key; ?>')">
                     Show table!
                 </button>
             </div>
@@ -108,6 +119,7 @@ require_once "./includes/header.php";
     <footer>
     </footer>
 </body>
+
 </html>
 
 <script src="./functions/badgeType.js"></script>
@@ -117,74 +129,74 @@ require_once "./includes/header.php";
 
 <script>
 
-let badgeTableVisible = false;
-let lotTableVisible = false;
-let timeTableVisible = false;
-let ruleTableVisible = false;
+    let badgeTableVisible = false;
+    let lotTableVisible = false;
+    let timeTableVisible = false;
+    let ruleTableVisible = false;
 
-function badgeTableVisibility(url, api) {
-    const table = document.getElementById('badgeTable');
-    const button = document.getElementById('showBadge');
+    function badgeTableVisibility(url, api) {
+        const table = document.getElementById('badgeTable');
+        const button = document.getElementById('showBadge');
 
-    if (badgeTableVisible) {
-        table.style.display = 'none';
-        button.textContent = 'Show Table!';
-    } else {
-        // Show the table (assuming showLot function does this)
-        showBadge(url, api);
-        table.style.display = 'flex';
-        button.textContent = 'Hide Table!';
+        if (badgeTableVisible) {
+            table.style.display = 'none';
+            button.textContent = 'Show Table!';
+        } else {
+            // Show the table (assuming showLot function does this)
+            showBadge(url, api);
+            table.style.display = 'flex';
+            button.textContent = 'Hide Table!';
+        }
+        badgeTableVisible = !badgeTableVisible;
     }
-    badgeTableVisible = !badgeTableVisible;
-}
 
-function lotTableVisibility(url, api) {
-    const table = document.getElementById('lotTable');
-    const button = document.getElementById('showLot');
+    function lotTableVisibility(url, api) {
+        const table = document.getElementById('lotTable');
+        const button = document.getElementById('showLot');
 
-    if (lotTableVisible) {
-        table.style.display = 'none';
-        button.textContent = 'Show Table!';
-    } else {
-        // Show the table (assuming showLot function does this)
-        showLot(url, api);
-        table.style.display = 'flex';
-        button.textContent = 'Hide Table!';
+        if (lotTableVisible) {
+            table.style.display = 'none';
+            button.textContent = 'Show Table!';
+        } else {
+            // Show the table (assuming showLot function does this)
+            showLot(url, api);
+            table.style.display = 'flex';
+            button.textContent = 'Hide Table!';
+        }
+        lotTableVisible = !lotTableVisible;
     }
-    lotTableVisible = !lotTableVisible;
-}
 
-function timeTableVisibility(url, api) {
-    const table = document.getElementById('timeTable');
-    const button = document.getElementById('showTime');
+    function timeTableVisibility(url, api) {
+        const table = document.getElementById('timeTable');
+        const button = document.getElementById('showTime');
 
-    if (timeTableVisible) {
-        table.style.display = 'none';
-        button.textContent = 'Show Table!';
-    } else {
-        // Show the table (assuming showLot function does this)
-        showTime(url, api);
-        table.style.display = 'flex';
-        button.textContent = 'Hide Table!';
+        if (timeTableVisible) {
+            table.style.display = 'none';
+            button.textContent = 'Show Table!';
+        } else {
+            // Show the table (assuming showLot function does this)
+            showTime(url, api);
+            table.style.display = 'flex';
+            button.textContent = 'Hide Table!';
+        }
+        timeTableVisible = !timeTableVisible;
     }
-    timeTableVisible = !timeTableVisible;
-}
 
-function ruleTableVisibility(url, api) {
-    const table = document.getElementById('ruleTable');
-    const button = document.getElementById('showRule');
+    function ruleTableVisibility(url, api) {
+        const table = document.getElementById('ruleTable');
+        const button = document.getElementById('showRule');
 
-    if (ruleTableVisible) {
-        table.style.display = 'none';
-        button.textContent = 'Show Table!';
-    } else {
-        // Show the table (assuming showLot function does this)
-        showRule(url, api);
-        table.style.display = 'flex';
-        button.textContent = 'Hide Table!';
+        if (ruleTableVisible) {
+            table.style.display = 'none';
+            button.textContent = 'Show Table!';
+        } else {
+            // Show the table (assuming showLot function does this)
+            showRule(url, api);
+            table.style.display = 'flex';
+            button.textContent = 'Hide Table!';
+        }
+        ruleTableVisible = !ruleTableVisible;
     }
-    ruleTableVisible = !ruleTableVisible;
-}
 
 
 </script>
